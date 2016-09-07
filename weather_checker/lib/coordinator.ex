bd650b3af4e17701734b4f43a4cb82d79c0479db
mxchan <mingxiangchan@gmail.com>
2016-09-07 10:18:42 +0800

    completed weather checker

--- weather_checker/lib/coordinator.ex
+++ weather_checker/lib/coordinator.ex
@@ -1,12 +1,12 @@
 defmodule WeatherChecker.Coordinator do
   def loop(results \\ [], results_expected) do
     receive do
-      {:ok, results} ->
+      {:ok, result} ->
         new_results = [result|results]
         if results_expected == Enum.count(new_results) do
           send self, :exit
         end
-        loop(new_resuts, results_expected)
+        loop(new_results, results_expected)
       :exit ->
         IO.puts(results |> Enum.sort |> Enum.join(","))
        _ ->
--- weather_checker/lib/weather_checker.ex
+++ weather_checker/lib/weather_checker.ex
@@ -1,2 +1,10 @@
 defmodule WeatherChecker do
+  def temperatures_of(cities) do
+    coordinator_pid = spawn(WeatherChecker.Coordinator, :loop, [[], Enum.count(cities)])
+
+    cities |> Enum.each(fn city ->
+      worker_pid = spawn(WeatherChecker.Worker, :loop, [])
+     send worker_pid, {coordinator_pid, city}
+    end)
+  end
 end
