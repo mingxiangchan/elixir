b42c957e47e4ef1520e3e56f8150bfe172e70f77
mxchan <mingxiangchan@gmail.com>
2016-09-20 16:22:56 +0800

    able to create users

--- rumbl/web/models/user.ex
+++ rumbl/web/models/user.ex
@@ -1,3 +1,20 @@
 defmodule Rumbl.User do
-  defstruct [:id, :name, :username, :password]
+  use Rumbl.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps
+  end
+
+  def changeset(model, params \\ :empty) do
+    model
+    # tell Ecto "name" and "username" are required, empty array means no optional params
+    |> cast(params, ~w(name username), [])
+    |> validate_length(:username, min: 1, max: 20)
+  end
 end
+
